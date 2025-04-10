67a8137751257aeac794e35bc88d8b7a4bf6406d
bevilld <dbevill@unc.edu>
2025-04-09 22:15:01 -0400

    Progress on Exercise

--- exercises/ex04/river.py
+++ exercises/ex04/river.py
@@ -1,11 +1,15 @@
 """File to define River class."""
 
-from exercises.EX04.fish import Fish
-from exercises.EX04.bear import Bear
+from exercises.ex04.fish import Fish
+from exercises.ex04.bear import Bear
+
 
 class River:
-    
-    def __init__(self, num_fish: int, num_bears:int):
+    day: int
+    bears: list[Bear]
+    fish: list[Fish]
+
+    def __init__(self, num_fish: int, num_bears: int):
         """New River with num_fish Fish and num_bears Bears"""
         self.day: int = 0
         self.fish: list[Fish] = []
@@ -17,23 +21,62 @@ class River:
             self.bears.append(Bear())
 
     def check_ages(self):
+        bears: list[Bear] = self.bears
+        fish: list[Fish] = self.fish
+        for i in fish:
+            if i.age <= 3:
+                fish.append(i)
+        for i in bears:
+            if i.age <= 5:
+                bears.append(i)
+        self.bears = bears
+        self.fish = fish
+        return None
+
+    def remove_fish(self, amount: int):
+        idx: int = 0
+        while idx < amount:
+            self.fish.pop(0)
+            idx += 1
         return None
 
     def bears_eating(self):
+        for i in self.bears:
+            if len(self.fish) > 5:
+                self.remove_fish(3)
+                i.eat(3)
         return None
-    
+
     def check_hunger(self):
+        bears: list[Bear] = []
+        for i in self.bears:
+            if i.hunger_score >= 0:
+                bears.append(i)
+        self.bears = bears
         return None
-        
+
     def repopulate_fish(self):
+        idx: int = 0
+        spawn: int = (len(self.fish) // 2) * 4
+        while idx < spawn:
+            self.fish.append(Fish())
+            idx += 1
         return None
-    
+
     def repopulate_bears(self):
+        idx: int = 0
+        cubs: int = len(self.bears) // 2
+        while idx < cubs:
+            self.bears.append(Bear())
+            idx += 1
         return None
-    
+
     def view_river(self):
+        print(f"~~~ Day {self.day}: ~~~")
+        print(f"Fish population: {len(self.fish)}")
+        print(f"Bear population: {len(self.bears)}")
         return None
-            
+
     def one_river_day(self):
         """Simulate one day of life in the river"""
         # Increase day by 1
@@ -56,4 +99,10 @@ class River:
         self.repopulate_bears()
         # Visualize River
         self.view_river()
-            
+
+    def one_river_week(self):
+        idx = 0
+        while idx < 7:
+            self.one_river_day()
+            idx += 1
+        return None
--- hw.py
+++ hw.py
@@ -1,6 +1,44 @@
-ice_cream: dict[str, int] = {
-    "chocolate": 12,
-    "vanilla": 8,
-    "strawberry": 4,
-}
-print(ice_cream["mint"])
+def count_lens(words: list[str]) -> dict[int, int]:
+    idx: int = 0
+    counted: dict[int, int] = {}
+    while idx < len(words):
+        if len(words[idx]) in counted:
+            counted[len(words[idx])] += 1
+            idx += 1
+        else:
+            counted[len(words[idx])] = 1
+            idx += 1
+    return counted
+
+
+print(count_lens(["a", "b", "cc", "d"]))
+
+
+def count_lens_test() -> None:
+    assert count_lens(["a", "bb", "ccc", "dddd"]) == {1: 1, 2: 1, 3: 1, 4: 1}
+
+
+class Point:
+    x: float
+    y: float
+
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    start: Point
+    end: Point
+
+    def __init__(self, start: Point, end: Point):
+        self.start = start
+        self.end = end
+
+    def get_length(self) -> float:
+        return (
+            (self.end.x - self.start.x) ** 2 + (self.end.y - self.start.y) ** 2
+        ) ** 0.5
+
+    def get_slope(self) -> float:
+        return (self.end.y - self.start.y) / (self.end.x - self.start.x)
